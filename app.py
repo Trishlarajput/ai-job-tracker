576d09cc7ab7152f0000044f352401d554fc2efa
Trishla <trishlarajput15@gmail.com>
2026-07-18 19:34:46 +0530

    Fix db initialization gor gunicorn

--- app.py
+++ app.py
@@ -69,7 +69,10 @@ def analyze():
 
     return render_template('analyze.html', result=result)
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
+    
+    
